0a54f3909dea264403b1bf691b40ac51e17cf00c
kmrocki@us.ibm.com <kmrocki@us.ibm.com>
2017-03-06 20:40:29 -0800

    draw inputs, loss

--- src/gl/tex.h
+++ src/gl/tex.h
@@ -2,5 +2,7 @@
 * @Author: kmrocki@us.ibm.com
 * @Date:   2017-03-06 13:20:16
 * @Last Modified by:   kmrocki@us.ibm.com
-* @Last Modified time: 2017-03-06 19:50:01
+* @Last Modified time: 2017-03-06 20:40:26
 */
+
+// wrapper for tex2d
