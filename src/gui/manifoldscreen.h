128aa81e7603a583e922db28294a63274275a24f
kmrocki@us.ibm.com <kmrocki@us.ibm.com>
2017-03-14 21:34:15 -0700

    ray casting working

--- src/gui/manifoldscreen.h
+++ src/gui/manifoldscreen.h
@@ -12,186 +12,186 @@
 // nvgCreateImageA
 #include <gl/tex.h>
 
-#define DEF_WIDTH 1630
-#define DEF_HEIGHT 1266
+#define DEF_WIDTH 1430
+#define DEF_HEIGHT 1106
 #define SCREEN_NAME "AE"
 
 class GUI : public nanogui::Screen {
 
-	public:
-	
-		GUI ( ) :
-		
-			nanogui::Screen ( Eigen::Vector2i ( DEF_WIDTH / screen_scale, DEF_HEIGHT / screen_scale ), SCREEN_NAME ),
-			vsync ( true ) { init(); }
-			
-		void init() {
-		
-			/* get physical GLFW screen size */
-			glfwGetWindowSize ( glfwWindow(), &glfw_window_width, &glfw_window_height );
-			
-			// get GL capabilities info
-			printf ( "GL_RENDERER: %s\n", glGetString ( GL_RENDERER ) );
-			printf ( "GL_VERSION: %s\n", glGetString ( GL_VERSION ) );
-			printf ( "GLSL_VERSION: %s\n\n", glGetString ( GL_SHADING_LANGUAGE_VERSION ) );
-			
-			/* * create widgets  * */
-			
-			//make image placeholders
-			for ( size_t i = 0; i < batch_size; i++ ) {
-			
-				xs.emplace_back ( std::pair<int, std::string> ( nvgCreateImageA ( nvgContext(),
-								  image_size, image_size, NVG_IMAGE_NEAREST, ( unsigned char * ) nullptr ), "" ) );
-								  
-				ys.emplace_back ( std::pair<int, std::string> ( nvgCreateImageA ( nvgContext(),
-								  image_size, image_size, NVG_IMAGE_NEAREST, ( unsigned char * ) nullptr ), "" ) );
-								  
-			}
-			
-			nanogui::Window *images = new nanogui::Window ( this, "images" );
-			images->setLayout ( new nanogui::GroupLayout () );
-			
-			nanogui::ImagePanel *inp = new nanogui::ImagePanel ( images, 50 / screen_scale, 2 / screen_scale, 2 / screen_scale, {10, batch_size / 10} );
-			inp->setImages ( xs );
-			nanogui::ImagePanel *out = new nanogui::ImagePanel ( images, 50 / screen_scale, 2 / screen_scale, 2 / screen_scale, {10, batch_size / 10} );
-			out->setImages ( ys );
-			
-			nanogui::Window *plot = new nanogui::Window ( this, "" );
-			plot->setPosition ( { 585 / screen_scale, 15 / screen_scale } );
-			plot->setLayout ( new nanogui::GroupLayout() );
-			
-			/* * * * * * * * * * * */
-			
-			
-			// wait until nn is ready
-			while ( ! ( nn->isready() ) )
-				usleep ( 10000 );
-				
-				
-			mCanvas_helper = new SurfWindow ( plot, "" );
-			mCanvas_helper->setSize ( {1000, 100} );
-			mCanvas_helper->setLayout ( new nanogui::GroupLayout() );
-			
-			/* FPS GRAPH */
-			graph_fps = new nanogui::Graph ( mCanvas_helper, "" );
-			graph_fps->setGraphColor ( nanogui::Color ( 0, 160, 192, 255 ) );
-			graph_fps->setBackgroundColor ( nanogui::Color ( 0, 0, 0, 32 ) );
-			
-			mCanvas = new SurfPlot ( plot, {1000, 1000}, *mCanvas_helper );
-			mCanvas->graph_data = graph_fps->values_ptr();
-			mCanvas->setBackgroundColor ( {100, 100, 100, 64} );
-			
-			drawAll();
-			setVisible ( true );
-			
-			glfwSwapInterval ( vsync );
-			
-			performLayout();
-			resizeEvent ( { glfw_window_width, glfw_window_height } );
-			
-			
+  public:
+
+	GUI ( ) :
+
+		nanogui::Screen ( Eigen::Vector2i ( DEF_WIDTH / screen_scale, DEF_HEIGHT / screen_scale ), SCREEN_NAME ),
+		vsync ( true ) { init(); }
+
+	void init() {
+
+		/* get physical GLFW screen size */
+		glfwGetWindowSize ( glfwWindow(), &glfw_window_width, &glfw_window_height );
+
+		// get GL capabilities info
+		printf ( "GL_RENDERER: %s\n", glGetString ( GL_RENDERER ) );
+		printf ( "GL_VERSION: %s\n", glGetString ( GL_VERSION ) );
+		printf ( "GLSL_VERSION: %s\n\n", glGetString ( GL_SHADING_LANGUAGE_VERSION ) );
+
+		/* * create widgets  * */
+
+		//make image placeholders
+		for ( size_t i = 0; i < batch_size; i++ ) {
+
+			xs.emplace_back ( std::pair<int, std::string> ( nvgCreateImageA ( nvgContext(),
+			                  image_size, image_size, NVG_IMAGE_NEAREST, ( unsigned char * ) nullptr ), "" ) );
+
+			ys.emplace_back ( std::pair<int, std::string> ( nvgCreateImageA ( nvgContext(),
+			                  image_size, image_size, NVG_IMAGE_NEAREST, ( unsigned char * ) nullptr ), "" ) );
+
 		}
-		
-		
-		virtual void drawContents() {
-		
-			refresh();
-			
+
+		nanogui::Window *images = new nanogui::Window ( this, "images" );
+		images->setLayout ( new nanogui::GroupLayout () );
+
+		nanogui::ImagePanel *inp = new nanogui::ImagePanel ( images, 50 / screen_scale, 2 / screen_scale, 2 / screen_scale, {10, batch_size / 10} );
+		inp->setImages ( xs );
+		nanogui::ImagePanel *out = new nanogui::ImagePanel ( images, 50 / screen_scale, 2 / screen_scale, 2 / screen_scale, {10, batch_size / 10} );
+		out->setImages ( ys );
+
+		nanogui::Window *plot = new nanogui::Window ( this, "" );
+		plot->setPosition ( { 585 / screen_scale, 15 / screen_scale } );
+		plot->setLayout ( new nanogui::GroupLayout() );
+
+		/* * * * * * * * * * * */
+
+
+		// wait until nn is ready
+		while ( ! ( nn->isready() ) )
+			usleep ( 10000 );
+
+
+		mCanvas_helper = new SurfWindow ( plot, "" );
+		mCanvas_helper->setSize ( {800, 100} );
+		mCanvas_helper->setLayout ( new nanogui::GroupLayout() );
+
+		/* FPS GRAPH */
+		graph_fps = new nanogui::Graph ( mCanvas_helper, "" );
+		graph_fps->setGraphColor ( nanogui::Color ( 0, 160, 192, 255 ) );
+		graph_fps->setBackgroundColor ( nanogui::Color ( 0, 0, 0, 32 ) );
+
+		mCanvas = new SurfPlot ( plot, {800, 800}, *mCanvas_helper );
+		mCanvas->graph_data = graph_fps->values_ptr();
+		mCanvas->setBackgroundColor ( {100, 100, 100, 64} );
+
+		drawAll();
+		setVisible ( true );
+
+		glfwSwapInterval ( vsync );
+
+		performLayout();
+		resizeEvent ( { glfw_window_width, glfw_window_height } );
+
+
+	}
+
+
+	virtual void drawContents() {
+
+		refresh();
+
+	}
+
+	void refresh() {
+
+		Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> rgba_image;
+		rgba_image.resize ( image_size, image_size );
+
+		// xs
+		for ( size_t i = 0; i < nn->batch_size; i++ ) {
+
+			Eigen::MatrixXf float_image = nn->layers[0]->x.col ( i );
+
+			float_image.resize ( image_size, image_size );
+			float_image *= 255.0f;
+
+			rgba_image = float_image.cast<unsigned char>();
+			nvgUpdateImage ( nvgContext(), xs[i].first, ( unsigned char * ) rgba_image.data() );
+
+
 		}
-		
-		void refresh() {
-		
-			Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic> rgba_image;
-			rgba_image.resize ( image_size, image_size );
-			
-			// xs
-			for ( size_t i = 0; i < nn->batch_size; i++ ) {
-			
-				Eigen::MatrixXf float_image = nn->layers[0]->x.col ( i );
-				
-				float_image.resize ( image_size, image_size );
-				float_image *= 255.0f;
-				
-				rgba_image = float_image.cast<unsigned char>();
-				nvgUpdateImage ( nvgContext(), xs[i].first, ( unsigned char * ) rgba_image.data() );
-				
-				
-			}
-			
-			// ys
-			for ( size_t i = 0; i < nn->batch_size; i++ ) {
-			
-				Eigen::MatrixXf float_image = nn->layers.back()->y.col ( i );
-				
-				float_image.resize ( image_size, image_size );
-				float_image *= 255.0f;
-				
-				rgba_image = float_image.cast<unsigned char>();
-				nvgUpdateImage ( nvgContext(), ys[i].first, ( unsigned char * ) rgba_image.data() );
-				
-			}
-			
-			if ( mCanvas ) {
-				mCanvas->refresh();
-				if ( mCanvas->graph_data->size() > 0 )
-					graph_fps->setHeader ( string_format ( "%.1f FPS", mCanvas->graph_data->mean() ) );
-			}
-			
+
+		// ys
+		for ( size_t i = 0; i < nn->batch_size; i++ ) {
+
+			Eigen::MatrixXf float_image = nn->layers.back()->y.col ( i );
+
+			float_image.resize ( image_size, image_size );
+			float_image *= 255.0f;
+
+			rgba_image = float_image.cast<unsigned char>();
+			nvgUpdateImage ( nvgContext(), ys[i].first, ( unsigned char * ) rgba_image.data() );
+
 		}
-		
-		/* event handlers */
-		
-		virtual bool keyboardEvent ( int key, int scancode, int action, int modifiers ) {
-		
-			/* process subcomponents */
-			if ( Screen::keyboardEvent ( key, scancode, action, modifiers ) )
-				return true;
-				
-			if ( key == GLFW_KEY_SPACE && action == GLFW_PRESS )
-				nn->pause = !nn->pause;
-				
-			if ( key == GLFW_KEY_N && action == GLFW_PRESS ) {
-			
-				if ( nn->pause )
-					nn->step = true;
-					
-			}
-			
-			/* close */
-			if ( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS ) {
-			
-				nn->quit = true;
-				setVisible ( false );
-				
-				return true;
-			}
-			
-			return false;
+
+		if ( mCanvas ) {
+			mCanvas->refresh();
+			if ( mCanvas->graph_data->size() > 0 )
+				graph_fps->setHeader ( string_format ( "%.1f FPS", mCanvas->graph_data->mean() ) );
 		}
-		
-		virtual bool resizeEvent ( const Eigen::Vector2i &size ) {
-		
-			UNUSED ( size );
-			performLayout();
-			
+
+	}
+
+	/* event handlers */
+
+	virtual bool keyboardEvent ( int key, int scancode, int action, int modifiers ) {
+
+		/* process subcomponents */
+		if ( Screen::keyboardEvent ( key, scancode, action, modifiers ) )
 			return true;
-			
+
+		if ( key == GLFW_KEY_SPACE && action == GLFW_PRESS )
+			nn->pause = !nn->pause;
+
+		if ( key == GLFW_KEY_N && action == GLFW_PRESS ) {
+
+			if ( nn->pause )
+				nn->step = true;
+
 		}
-		
-		~GUI() { /* free resources */}
-		
-	protected:
-	
-		int glfw_window_width, glfw_window_height;
-		bool vsync;
-		
-		SurfWindow *mCanvas_helper;
-		SurfPlot *mCanvas;
-		nanogui::Graph *graph_fps;
-		
-		using imagesDataType = std::vector<std::pair<int, std::string>>;
-		imagesDataType xs, ys;
-		
+
+		/* close */
+		if ( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS ) {
+
+			nn->quit = true;
+			setVisible ( false );
+
+			return true;
+		}
+
+		return false;
+	}
+
+	virtual bool resizeEvent ( const Eigen::Vector2i &size ) {
+
+		UNUSED ( size );
+		performLayout();
+
+		return true;
+
+	}
+
+	~GUI() { /* free resources */}
+
+  protected:
+
+	int glfw_window_width, glfw_window_height;
+	bool vsync;
+
+	SurfWindow *mCanvas_helper;
+	SurfPlot *mCanvas;
+	nanogui::Graph *graph_fps;
+
+	using imagesDataType = std::vector<std::pair<int, std::string>>;
+	imagesDataType xs, ys;
+
 };
 
 #endif
--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,8 @@
 /*
 * @Author: Kamil Rocki
 * @Date:   2017-02-28 11:25:34
-* @Last Modified by:   Kamil Rocki
-* @Last Modified time: 2017-03-14 17:09:21
+* @Last Modified by:   kmrocki@us.ibm.com
+* @Last Modified time: 2017-03-14 21:06:49
 */
 
 #include <thread>
@@ -35,90 +35,89 @@ int compute() {
 
 	// TODO: be able to change batch size, learning rate and decay dynamically
 	// serialization
-	
+
 	double learning_rate = 1e-4;
 	float decay = 1e-5;
-	
-	std::vector<int> layer_sizes = {image_size * image_size, 100, 100, 100, 100, 3, 100, 100, 100, 100, image_size * image_size};
+
+	std::vector<int> layer_sizes = {image_size * image_size, 100, 3, 100, image_size * image_size};
 	nn = new NN ( batch_size, decay, AE );
-	
-	nn->code_layer_no = 9;
-	
+
+	nn->code_layer_no = 3;
+
 	size_t l = 0;
-	
+
 	for ( l = 0; l < layer_sizes.size() - 1; l++ ) {
-	
+
 		nn->layers.push_back ( new Linear ( layer_sizes[l], layer_sizes[l + 1], batch_size ) );
-		
+
 		if ( ( l + 1 ) == layer_sizes.size() - 1 )
 			nn->layers.push_back ( new Sigmoid ( layer_sizes[l + 1], layer_sizes[l + 1], batch_size ) );
 		else
 			nn->layers.push_back ( new ReLU ( layer_sizes[l + 1], layer_sizes[l + 1], batch_size ) );
 	}
-	
+
 	//[60000, 784]
 	std::deque<datapoint> train_data =
-		MNISTImporter::importFromFile ( "data/mnist/train-images-idx3-ubyte", "data/mnist/train-labels-idx1-ubyte" );
-		
+	    MNISTImporter::importFromFile ( "data/mnist/train-images-idx3-ubyte", "data/mnist/train-labels-idx1-ubyte" );
+
 	//[10000, 784]
 	std::deque<datapoint> test_data =
-		MNISTImporter::importFromFile ( "data/mnist/t10k-images-idx3-ubyte", "data/mnist/t10k-labels-idx1-ubyte" );
-		
+	    MNISTImporter::importFromFile ( "data/mnist/t10k-images-idx3-ubyte", "data/mnist/t10k-labels-idx1-ubyte" );
+
 	nn->testcode ( train_data );
-	
+
 	std::cout << "nn ready" << std::endl;
 	nn->setready();
-	
+
 	for ( size_t e = 0; true; e++ ) {
-	
+
 		nn->train ( train_data, learning_rate, train_data.size() / batch_size );
 		nn->testcode ( train_data );
-		
+
 		if ( nn->quit ) break;
-		
+
 		printf ( "Epoch %3lu: Loss: %.2f\n", e + 1, ( float ) nn->test ( test_data ) );
-		
+
 	}
-	
+
 	nn->quit = true;
-	
+
 	//should wait for GL to finish first before deleting nn
 	delete nn; return 0;
-	
+
 }
 
 int main ( int /* argc */, char ** /* argv */ ) {
 
 	try {
-	
+
 		/* init GUI */
 		nanogui::init();
-		
+
 		// launch a compute thread
 		std::thread compute_thread ( compute );
-		
+
 		// wait until nn is allocated
 		while ( ! nn ) {
 			std::cout << "main" << std::endl;
 			usleep ( 1000 );
 		}
-		
+
 		screen = new GUI();
-		
+
 		nanogui::mainloop ( 1 );
-		
+
 		delete screen;
 		nanogui::shutdown();
 		compute_thread.join();
-		
-	}
-	catch ( const std::runtime_error &e ) {
-	
+
+	} catch ( const std::runtime_error &e ) {
+
 		std::string error_msg = std::string ( "Caught a fatal error: " ) + std::string ( e.what() );
 		return -1;
-		
+
 	}
-	
+
 	return 0;
-	
+
 }
