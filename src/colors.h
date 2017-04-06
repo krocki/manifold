5270806c1d910021e4ab887a0c70d957adefcac4
Kamil Rocki <kmrocki@us.ibm.com>
2017-04-06 14:38:43 -0700

    fixed mW, mb, elu, prog_bar

--- src/colors.h
+++ src/colors.h
@@ -1,43 +1,130 @@
-_float4 haxby_colormap(float value) {
 
-    _float4 color;
+/*
+* @Author: kmrocki@us.ibm.com
+* @Date:   2017-03-09 22:20:32
+* @Last Modified by:   Kamil Rocki
+* @Last Modified time: 2017-04-06 11:14:47
+*/
 
-    unsigned low = (unsigned)(float)floor(value * (haxby_colors - 1));
-    unsigned high = (unsigned)(float)ceil(value * (haxby_colors - 1));
+#include <nanogui/colormaps.h>
 
-    float v = (float)(haxby_colors - 1) * (value - (float)floor(value * (haxby_colors - 1)) / (haxby_colors - 1));
+#ifndef __COLORS_H__
+#define __COLORS_H__
 
-    float r = (float)linear_interpolation( v, haxby[low][0], haxby[high][0] );
-    float g = (float)linear_interpolation( v, haxby[low][1], haxby[high][1] );
-    float b = (float)linear_interpolation( v, haxby[low][2], haxby[high][2] );
+float interpolate_jet ( float val, float y0, float x0, float y1, float x1 ) {
 
-    color.r = r / 255.0f;
-    color.g = g / 255.0f;
-    color.b = b / 255.0f;
-    color.a = 1.0f;
-
-    return color;
-
+	return ( val - x0 ) * ( y1 - y0 ) / ( x1 - x0 ) + y0;
 }
 
-_float4 seismic_colormap(float value) {
+float jet_base ( float val ) {
 
-    _float4 color;
-
-    unsigned low = (unsigned)(float)floor(value * (seismic_colors - 1));
-    unsigned high = (unsigned)(float)ceil(value * (seismic_colors - 1));
-
-    float v = (float)(seismic_colors - 1) * (value - (float)floor(value * (seismic_colors - 1)) / (seismic_colors - 1));
-
-    float r = (float)linear_interpolation( v, seismic[low][0], seismic[high][0] );
-    float g = (float)linear_interpolation( v, seismic[low][1], seismic[high][1] );
-    float b = (float)linear_interpolation( v, seismic[low][2], seismic[high][2] );
-
-    color.r = r / 255.0f;
-    color.g = g / 255.0f;
-    color.b = b / 255.0f;
-    color.a = 1.0f;
-
-    return color;
-
+	if ( val <= -0.75f ) return 0;
+	else
+		if ( val <= -0.25f ) return interpolate_jet ( val, 0.0f, -0.75f, 1.0f, -0.25f );
+		else
+			if ( val <= 0.25f ) return 1.0f;
+			else
+				if ( val <= 0.75f ) return interpolate_jet ( val, 1.0f, 0.25f, 0.0f, 0.75f );
+				else return 0.0f;
+				
 }
+
+float jet_red ( float value ) {
+
+	return jet_base ( value - 0.6f );
+}
+
+float jet_green ( float value ) {
+
+	return jet_base ( value );
+}
+
+float jet_blue ( float value ) {
+
+	return jet_base ( value + 0.6f );
+}
+
+
+nanogui::Color jet_colormap ( float value ) {
+
+	value *= 2.0f;
+	value -= 1.0f;
+	
+	//MATLAB's jet palette
+	float r = jet_red ( value );
+	float g = jet_green ( value );
+	float b = jet_blue ( value );
+	
+	return nanogui::Color ( ( uint8_t ) ( r * 255.0f ), ( uint8_t ) ( g * 255.0f ), ( uint8_t ) ( b * 255.0f ), 127 );
+}
+
+
+nanogui::Color parula_colormap ( float value ) {
+
+	//simple discretization, NN
+	int index = round ( value * 10.0f - 0.5f );
+	
+	index = fmin ( index, nanogui::PARULA_LUT_SIZE - 1 );
+	index = fmax ( index, 0 );
+	
+	return nanogui::parula_lut[index];
+	
+}
+
+nanogui::Color bar_colormap ( float value ) {
+
+	//simple discretization, NN
+	int index = round ( value * 10.0f - 0.5f );
+	
+	index = fmin ( index, nanogui::BARCOLOR_LUT_SIZE - 1 );
+	index = fmax ( index, 0 );
+	
+	return nanogui::barcolormap_lut[index];
+	
+}
+
+// _float4 haxby_colormap(float value) {
+
+//     _float4 color;
+
+//     unsigned low = (unsigned)(float)floor(value * (haxby_colors - 1));
+//     unsigned high = (unsigned)(float)ceil(value * (haxby_colors - 1));
+
+//     float v = (float)(haxby_colors - 1) * (value - (float)floor(value * (haxby_colors - 1)) / (haxby_colors - 1));
+
+//     float r = (float)linear_interpolation( v, haxby[low][0], haxby[high][0] );
+//     float g = (float)linear_interpolation( v, haxby[low][1], haxby[high][1] );
+//     float b = (float)linear_interpolation( v, haxby[low][2], haxby[high][2] );
+
+//     color.r = r / 255.0f;
+//     color.g = g / 255.0f;
+//     color.b = b / 255.0f;
+//     color.a = 1.0f;
+
+//     return color;
+
+// }
+
+// _float4 seismic_colormap(float value) {
+
+//     _float4 color;
+
+//     unsigned low = (unsigned)(float)floor(value * (seismic_colors - 1));
+//     unsigned high = (unsigned)(float)ceil(value * (seismic_colors - 1));
+
+//     float v = (float)(seismic_colors - 1) * (value - (float)floor(value * (seismic_colors - 1)) / (seismic_colors - 1));
+
+//     float r = (float)linear_interpolation( v, seismic[low][0], seismic[high][0] );
+//     float g = (float)linear_interpolation( v, seismic[low][1], seismic[high][1] );
+//     float b = (float)linear_interpolation( v, seismic[low][2], seismic[high][2] );
+
+//     color.r = r / 255.0f;
+//     color.g = g / 255.0f;
+//     color.b = b / 255.0f;
+//     color.a = 1.0f;
+
+//     return color;
+
+// }
+
+#endif
--- src/nn/nn_utils.h
+++ src/nn/nn_utils.h
@@ -1,8 +1,8 @@
 /*
 * @Author: kmrocki
 * @Date:   2016-02-24 10:47:03
-* @Last Modified by:   kamilrocki
-* @Last Modified time: 2017-04-05 22:24:27
+* @Last Modified by:   Kamil Rocki
+* @Last Modified time: 2017-04-06 11:52:38
 */
 
 #ifndef __NN_UTILS_H__
@@ -24,23 +24,23 @@ typedef Eigen::VectorXf Vector;
 #include "perf.h"
 #include "aux.h"
 
-inline float sqrt_eps(const float x) {
-	return sqrtf(x + 1e-6);
+inline float sqrt_eps ( const float x ) {
+	return sqrtf ( x + 1e-6 );
 }
 
 //f(x) = sigm(x)
-inline float __logistic(const float x) {
-	return 1.0f / (1.0f +::expf(-x));
+inline float __logistic ( const float x ) {
+	return 1.0f / ( 1.0f +::expf ( -x ) );
 }
 
-inline float __exponential(const float x) {
-	return expf(x);
+inline float __exponential ( const float x ) {
+	return expf ( x );
 }
 
 #ifdef USE_BLAS
 #include <cblas.h>
-void BLAS_mmul( Eigen::MatrixXf& __restrict c, Eigen::MatrixXf& __restrict a,
-                Eigen::MatrixXf& __restrict b, bool aT = false, bool bT = false );
+void BLAS_mmul ( Eigen::MatrixXf &__restrict c, Eigen::MatrixXf &__restrict a,
+				 Eigen::MatrixXf &__restrict b, bool aT = false, bool bT = false );
 
 #endif
 
@@ -48,123 +48,117 @@ void random_binary_mask ( Matrix &mask ) {
 
 	for ( int i = 0; i < mask.rows(); i++ ) {
 		for ( int j = 0; j < mask.cols(); j++ )
-
+		
 			mask ( i, j ) = round ( rand_float ( 0, 1 ) );
-
+			
 	}
 }
 
 
-Matrix rectify(Matrix& x) {
+Matrix rectify ( Matrix &x ) {
 
-	Matrix y(x.rows(), x.cols());
-
-	for (int i = 0; i < x.rows(); i++) {
-		for (int j = 0; j < x.cols(); j++) {
-
-			y(i, j) = x(i, j) > 0.0f ? x(i, j) : 0.0f;
-		}
+	Matrix y ( x.rows(), x.cols() );
+	
+	for ( int i = 0; i < x.rows(); i++ ) {
+		for ( int j = 0; j < x.cols(); j++ )
+		
+			y ( i, j ) = x ( i, j ) > 0.0f ? x ( i, j ) : 0.0f;
 	}
-
+	
 	return y;
-
+	
 }
 
 // Exponential Linear Unit
 // http://arxiv.org/pdf/1511.07289v5.pdf
-Matrix activation_ELU(Matrix& x) {
+Matrix activation_ELU ( Matrix &x ) {
 
 	float alpha = 1.0f;
-
-	Matrix y(x.rows(), x.cols());
-
-	for (int i = 0; i < x.rows(); i++) {
-		for (int j = 0; j < x.cols(); j++) {
-
-			y(i, j) = x(i, j) >= 0.0f ? x(i, j) : alpha * (expf(x(i, j)) - 1.0f);
-
-		}
+	
+	Matrix y ( x.rows(), x.cols() );
+	
+	for ( int i = 0; i < x.rows(); i++ ) {
+		for ( int j = 0; j < x.cols(); j++ )
+		
+			y ( i, j ) = x ( i, j ) >= 0.0f ? x ( i, j ) : alpha * ( expf ( x ( i, j ) ) - 1.0f );
+			
 	}
-
+	
 	return y;
-
+	
 }
 
-Matrix derivative_ELU(Matrix& x) {
+Matrix derivative_ELU ( Matrix &x ) {
 
 	float alpha = 1.0f;
-
-	Matrix y(x.rows(), x.cols());
-
-	for (int i = 0; i < x.rows(); i++) {
-		for (int j = 0; j < x.cols(); j++) {
-
-			y(i, j) = x(i, j) >= 0.0f ? 1.0f : (expf(x(i, j)) + alpha);
-		}
+	
+	Matrix y ( x.rows(), x.cols() );
+	
+	for ( int i = 0; i < x.rows(); i++ ) {
+		for ( int j = 0; j < x.cols(); j++ )
+		
+			y ( i, j ) = x ( i, j ) >= 0.0f ? 1.0f : ( x ( i, j ) + alpha );
 	}
-
+	
 	return y;
-
+	
 }
 
-Matrix derivative_ReLU(Matrix& x) {
+Matrix derivative_ReLU ( Matrix &x ) {
 
-	Matrix y(x.rows(), x.cols());
-
-	for (int i = 0; i < x.rows(); i++) {
-		for (int j = 0; j < x.cols(); j++) {
-
-			y(i, j) = (float)(x(i, j) > 0);
-		}
+	Matrix y ( x.rows(), x.cols() );
+	
+	for ( int i = 0; i < x.rows(); i++ ) {
+		for ( int j = 0; j < x.cols(); j++ )
+		
+			y ( i, j ) = ( float ) ( x ( i, j ) > 0 );
 	}
-
+	
 	return y;
-
+	
 }
 
-Matrix logistic(Matrix& x) {
+Matrix logistic ( Matrix &x ) {
 
-	Matrix y(x.rows(), x.cols());
-
-	for (int i = 0; i < x.rows(); i++) {
-		for (int j = 0; j < x.cols(); j++) {
-
-			y(i, j) = __logistic(x(i, j));
-		}
+	Matrix y ( x.rows(), x.cols() );
+	
+	for ( int i = 0; i < x.rows(); i++ ) {
+		for ( int j = 0; j < x.cols(); j++ )
+		
+			y ( i, j ) = __logistic ( x ( i, j ) );
 	}
-
+	
 	return y;
 }
 
-Matrix softmax(Matrix& x) {
+Matrix softmax ( Matrix &x ) {
 
-	Matrix y(x.rows(), x.cols());
-
+	Matrix y ( x.rows(), x.cols() );
+	
 	//probs(class) = exp(x, class)/sum(exp(x, class))
-
-	Matrix e = x.unaryExpr(std::ptr_fun(::expf));
-
+	
+	Matrix e = x.unaryExpr ( std::ptr_fun ( ::expf ) );
+	
 	Vector sum = e.colwise().sum();
-
-	for (int i = 0; i < e.rows(); i++) {
-		for (int j = 0; j < e.cols(); j++) {
-
-			y(i, j) = e(i, j) / sum(j);
-		}
+	
+	for ( int i = 0; i < e.rows(); i++ ) {
+		for ( int j = 0; j < e.cols(); j++ )
+		
+			y ( i, j ) = e ( i, j ) / sum ( j );
 	}
-
+	
 	return y;
 }
 
-float cross_entropy(Matrix& predictions, Matrix& targets) {
+float cross_entropy ( Matrix &predictions, Matrix &targets ) {
 
 	float ce = 0.0f;
-	Matrix error(predictions.rows(), predictions.cols());
-
+	Matrix error ( predictions.rows(), predictions.cols() );
+	
 	//check what has happened and get information content for that event
-	error.array() = -predictions.unaryExpr(std::ptr_fun(::logf)).array() * targets.array();
+	error.array() = -predictions.unaryExpr ( std::ptr_fun ( ::logf ) ).array() * targets.array();
 	ce = error.sum();
-
+	
 	return ce;
 }
 
@@ -172,11 +166,11 @@ float mse ( Matrix &yhat, Matrix &y ) {
 
 	float mse = 0.0;
 	Matrix error = y - yhat;
-
+	
 	//check what has happened and get information content for that event
 	error.array() = 2.0f * error.array() * error.array();
 	mse = error.sum();
-
+	
 	return mse;
 }
 
@@ -186,10 +180,10 @@ void matrix_randi ( Eigen::VectorXi &m, int range_min, int range_max ) {
 	std::random_device rd;
 	std::mt19937 mt ( rd() );
 	std::uniform_int_distribution<> dis ( range_min, range_max );
-
+	
 	for ( int i = 0; i < m.rows(); i++ )
 		m ( i ) = ( float ) dis ( mt );
-
+		
 }
 
 //generate an array of random numbers in range
@@ -198,14 +192,13 @@ void matrix_rand ( Matrix &m, float range_min, float range_max ) {
 	std::random_device rd;
 	std::mt19937 mt ( rd() );
 	std::uniform_real_distribution<> randf ( range_min, range_max );
-
+	
 	for ( int i = 0; i < m.rows(); i++ ) {
-		for ( int j = 0; j < m.cols(); j++ ) {
+		for ( int j = 0; j < m.cols(); j++ )
 			m ( i, j ) = randf ( mt );
-		}
-
+			
 	}
-
+	
 }
 
 void matrix_randn ( Matrix &m, float mean, float stddev ) {
@@ -213,84 +206,85 @@ void matrix_randn ( Matrix &m, float mean, float stddev ) {
 	std::random_device rd;
 	std::mt19937 mt ( rd() );
 	std::normal_distribution<> randn ( mean, stddev );
-
+	
 	for ( int i = 0; i < m.rows(); i++ ) {
 		for ( int j = 0; j < m.cols(); j++ )
 			m ( i, j ) = randn ( mt );
 	}
-
+	
 }
 
 void linspace ( Eigen::VectorXi &m, int range_min, int range_max ) {
 
 	// not really linspace - fixed increment = 1, TODO - fix, use range_max
 	UNUSED ( range_max );
-
+	
 	for ( int i = 0; i < m.rows(); i++ )
 		m ( i ) = ( float ) ( range_min + i );
-
+		
 }
 
 void make_batch ( Matrix &batch, const std::deque<datapoint> &data, const Eigen::VectorXi &random_numbers ) {
 
 	size_t batch_size = random_numbers.rows();
-
+	
 	for ( size_t i = 0; i < batch_size; i++ )
-
+	
 		batch.col ( i ) = data[random_numbers ( i )].x;
-
-
+		
+		
 }
 
-void make_targets ( Matrix &targets, const Matrix &encoding, const std::deque<datapoint> &data, Eigen::VectorXi &random_numbers ) {
-
+void make_targets ( Matrix &targets, const Matrix &encoding, const std::deque<datapoint> &data,
+					Eigen::VectorXi &random_numbers ) {
+					
 	size_t batch_size = random_numbers.rows();
-
+	
 	for ( size_t i = 0; i < ( size_t ) batch_size; i++ )
-
+	
 		targets.col ( i ) = encoding.col ( data[random_numbers ( i )].y );
-
-
+		
+		
 }
 
 Eigen::VectorXi colwise_max_index ( Matrix &m ) {
 
 	Eigen::VectorXi indices ( m.cols() );
-
+	
 	for ( size_t i = 0; i < ( size_t ) m.cols(); i++ ) {
-
+	
 		float current_max_val;
 		int index;
-
+		
 		for ( size_t j = 0; j < ( size_t ) m.rows(); j++ ) {
-
+		
 			if ( j == 0 || m ( j, i ) > current_max_val ) {
-
+			
 				index = j;
 				current_max_val = m ( j, i );
 			}
-
+			
 			indices ( i ) = index;
-
+			
 		}
 	}
-
+	
 	return indices;
 }
 
 size_t count_zeros ( Eigen::VectorXi &m ) {
 
 	size_t zeros = 0;
-
+	
 	for ( int i = 0; i < m.rows(); i++ ) {
-
+	
 		bool isZero = m ( i ) == 0;
-
+		
 		zeros += isZero;
 	}
-
+	
 	return zeros;
-
+	
 }
 
 size_t count_correct_predictions ( Matrix &p, Matrix &t ) {
@@ -298,33 +292,33 @@ size_t count_correct_predictions ( Matrix &p, Matrix &t ) {
 	Eigen::VectorXi predicted_classes = colwise_max_index ( p );
 	Eigen::VectorXi target_classes = colwise_max_index ( t );
 	Eigen::VectorXi correct = ( target_classes - predicted_classes );
-
+	
 	return count_zeros ( correct );
 }
 
 #ifdef USE_BLAS
 // c = a * b
 void BLAS_mmul ( Eigen::MatrixXf &__restrict c, Eigen::MatrixXf &__restrict a,
-                 Eigen::MatrixXf &__restrict b, bool aT, bool bT ) {
-
+				 Eigen::MatrixXf &__restrict b, bool aT, bool bT ) {
+				 
 	enum CBLAS_TRANSPOSE transA = aT ? CblasTrans : CblasNoTrans;
 	enum CBLAS_TRANSPOSE transB = bT ? CblasTrans : CblasNoTrans;
-
+	
 	size_t M = c.rows();
 	size_t N = c.cols();
 	size_t K = aT ? a.rows() : a.cols();
-
+	
 	float alpha = 1.0f;
 	float beta = 1.0f;
-
+	
 	size_t lda = aT ? K : M;
 	size_t ldb = bT ? N : K;
 	size_t ldc = M;
-
-	cblas_sgemm( CblasColMajor, transA, transB, M, N, K, alpha,
-	             a.data(), lda,
-	             b.data(), ldb, beta, c.data(), ldc );
-
+	
+	cblas_sgemm ( CblasColMajor, transA, transB, M, N, K, alpha,
+				  a.data(), lda,
+				  b.data(), ldb, beta, c.data(), ldc );
+				  
 	flops_performed += 2 * M * N * K;
 	bytes_read += ( a.size() + b.size() ) * sizeof ( dtype );
 }
