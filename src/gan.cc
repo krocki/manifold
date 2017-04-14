f382d2c03662f2d218426bec2720d763abbe422c
Kamil Rocki <kmrocki@us.ibm.com>
2017-04-13 20:03:18 -0700

    tau

--- src/gan.cc
+++ src/gan.cc
@@ -1,8 +1,8 @@
 /*
 * @Author: Kamil Rocki
 * @Date:   2017-02-28 11:25:34
-* @Last Modified by:   kmrocki@us.ibm.com
-* @Last Modified time: 2017-04-13 16:18:13
+* @Last Modified by:   Kamil Rocki
+* @Last Modified time: 2017-04-13 19:51:08
 */
 
 #include <thread>
@@ -29,7 +29,7 @@ typedef struct datapoints {
 
 	Eigen::MatrixXf x;
 	Eigen::MatrixXf y;
-
+	
 } datapoints;
 
 typedef struct gan_train_data_type {
@@ -37,13 +37,13 @@ typedef struct gan_train_data_type {
 	datapoints noise, noise_n;
 	datapoints gen, real, mixed;
 	datapoints generator_dy;
-
+	
 	Eigen::MatrixXf generated_mask, real_mask;
 	Eigen::MatrixXi training_set_indices, mix_indices;
-
+	
 	bool rgba = false;
 	size_t image_size = 28;
-
+	
 } gan_train_data_type;
 
 gan_train_data_type gan_train_data;
@@ -58,50 +58,51 @@ void update_graph_data ( Eigen::VectorXf *data, float new_val ) {
 
 
 	if ( data ) {
-
+	
 		data->head ( data->size() - 1 ) = data->tail ( data->size() - 1 );
 		data->tail ( 1 ) ( 0 ) = new_val;
-
+		
 	}
-
-
+	
+	
 }
 int compute() {
 
 	std::chrono::time_point<std::chrono::system_clock> start, end;
 	start = std::chrono::system_clock::now();
-
+	
 	PlotData *gl_data = screen->plot_data;
-
+	
 	// NN stuff
 	double learning_rate = 1e-3;
 	float decay = 0;
-	const size_t batch_size = 100;
+	const size_t batch_size = 25;
 	const int input_width = static_cast<int> ( train_data[0].x.size() );
 	assert ( input_width > 0 );
-
+	
 	size_t e = 0;
-
-	nn = std::shared_ptr<NN> ( new NN ( batch_size, decay, learning_rate, AE, {3, 25, input_width }, RELU ) );
-	discriminator = std::shared_ptr<NN> ( new NN ( batch_size, decay, learning_rate, MLP, {input_width, 25, 1}, SIGMOID ) );
-
+	
+	nn = std::shared_ptr<NN> ( new NN ( batch_size, decay, learning_rate, AE, {3, 64, input_width }, RELU ) );
+	discriminator = std::shared_ptr<NN> ( new NN ( batch_size, decay, learning_rate, MLP, {input_width, 64, 1},
+												   SIGMOID ) );
+												   
 	nn->otype = SGD;
 	nn->pause = false;
 	discriminator->otype = SGD;
 	discriminator->pause = true;
-
+	
 	size_t generate_point_count = 10000;
 	generate ( std::uniform_real_distribution<> ( 0, 10 ),
-	           std::uniform_real_distribution<> ( 0, 10 ),
-	           std::uniform_real_distribution<> ( 0, 10 ),
-	           gl_data->s_vertices, generate_point_count, INDEPENDENT );
-
+			   std::uniform_real_distribution<> ( 0, 10 ),
+			   std::uniform_real_distribution<> ( 0, 10 ),
+			   gl_data->s_vertices, generate_point_count, INDEPENDENT );
+			   
 	func3::set ( {0.0f, 1.0f, 0.0f}, gl_data->s_colors, generate_point_count );
-
+	
 	if ( screen )
 		if ( screen->nnview )
 			screen->nnview->setnets ( nn, discriminator );
-
+			
 	//bind graph data
 	if ( screen ) {
 		if ( screen->graph_loss )
@@ -110,42 +111,42 @@ int compute() {
 			discriminator->real_acc_data = screen->graph_real_acc->values_ptr();
 		if ( screen->graph_fake_acc )
 			discriminator->fake_acc_data = screen->graph_fake_acc->values_ptr();
-
+			
 	}
-
+	
 	size_t iters = 0;
 	float generator_loss, smooth_generator_loss = -1.0f;
 	float discriminator_real_acc, smooth_discriminator_real_acc = -1.0f;
 	float discriminator_fake_acc, smooth_discriminator_fake_acc = -1.0f;
-
+	
 	/* work until main window is open */
-
+	
 	while ( screen->getVisible() ) {
-
+	
 		while ( nn->pause ) {
 			usleep ( 1000 );
 			if ( nn->step || nn->quit ) { nn->step = false; break; }
 		}
-
+		
 		// generate ( std::normal_distribution<> ( 0, 1 ), std::normal_distribution<> ( 0, 1 ),
 		// 		   std::normal_distribution<> ( 0, 1 ), gan_train_data.noise.x, batch_size, INDEPENDENT );
-
+		
 		// generate ( uniform_real_distribution<> ( 0, 1 ), uniform_real_distribution<> ( 0, 1 ),
-		//            uniform_real_distribution<> ( 0, 1 ), gan_train_data.noise.x, batch_size, INDEPENDENT );
-
-
-		generate ( std::normal_distribution<> ( 0, 1 ), std::normal_distribution<> ( 0, 1 ),
-		           std::normal_distribution<> ( 0, 1 ), gan_train_data.noise_n.x, batch_size, INDEPENDENT );
-
+		// 		   uniform_real_distribution<> ( 0, 1 ), gan_train_data.noise.x, batch_size, INDEPENDENT );
+		
+		
+		generate ( std::normal_distribution<> ( 0, 0.5 ), std::normal_distribution<> ( 0, 0.5 ),
+				   std::normal_distribution<> ( 0, 0.5 ), gan_train_data.noise_n.x, batch_size, INDEPENDENT );
+				   
 		gan_train_data.noise.x = gan_train_data.noise_n.x;
-
+		
 		nn->forward ( gan_train_data.noise.x );
-
+		
 		//generated data
 		gan_train_data.gen.x = nn->layers.back()->y;
 		gan_train_data.gen.y.resize ( 1, gan_train_data.gen.x.cols() );
 		gan_train_data.gen.y.setZero(); // set labels to 0 - images which came from generator
-
+		
 		//real data
 		gan_train_data.real.x.resize ( gan_train_data.gen.x.rows(), gan_train_data.gen.x.cols() );
 		gan_train_data.real.y.resize ( 1, gan_train_data.gen.x.cols() );
@@ -153,7 +154,7 @@ int compute() {
 		matrix_randi ( gan_train_data.training_set_indices, 0, train_data.size() - 1 );
 		make_batch ( gan_train_data.real.x, train_data, gan_train_data.training_set_indices );
 		gan_train_data.real.y.setOnes();
-
+		
 		//mix
 		gan_train_data.mix_indices.resize ( 1, batch_size );
 		matrix_randi ( gan_train_data.mix_indices, 0, 1 );
@@ -161,108 +162,114 @@ int compute() {
 		gan_train_data.mixed.y = gan_train_data.gen.y;
 		mix ( gan_train_data.mixed.x, gan_train_data.real.x, gan_train_data.mix_indices );
 		mix ( gan_train_data.mixed.y, gan_train_data.real.y, gan_train_data.mix_indices );
-
+		
 		// discriminator forward pass
 		discriminator->forward ( gan_train_data.mixed.x );
-
+		
 		// loss
 		gan_train_data.real_mask = gan_train_data.mix_indices.cast<float>();
 		generator_loss =
-		    0;//cross_entropy_mask ( discriminator->layers.back()->y, gan_train_data.mixed.y, gan_train_data.real_mask, true );
-
+			0;//cross_entropy_mask ( discriminator->layers.back()->y, gan_train_data.mixed.y, gan_train_data.real_mask, true );
+			
 		// discriminator backward pass
 		discriminator->backward ( gan_train_data.mixed.y - discriminator->layers.back()->y );
-
+		
 		// loss
 		discriminator_real_acc = cross_entropy_mask ( discriminator->layers.back()->y, gan_train_data.mixed.y,
-		                         gan_train_data.real_mask );
-		smooth_discriminator_real_acc = smooth_discriminator_real_acc < 0 ? discriminator_real_acc : 0.99 *
-		                                smooth_discriminator_real_acc + 0.01
-		                                * discriminator_real_acc;
-
-
+													  gan_train_data.real_mask );
+													  
+		smooth_discriminator_real_acc = isNaNInf ( discriminator_real_acc ) ? smooth_discriminator_real_acc :
+										smooth_discriminator_real_acc < 0 ? discriminator_real_acc : 0.99 *
+										smooth_discriminator_real_acc + 0.01
+										* discriminator_real_acc;
+										
+										
 		// loss 2
-
+		
 		//invert mask
 		gan_train_data.generated_mask = ( 1.0f - gan_train_data.mix_indices.cast<float>().array() );
 		generator_loss += cross_entropy_mask ( discriminator->layers.back()->y, gan_train_data.mixed.y,
-		                                       gan_train_data.generated_mask, false );
-
+											   gan_train_data.generated_mask, false );
+											   
 		discriminator_fake_acc = cross_entropy_mask ( discriminator->layers.back()->y, gan_train_data.mixed.y,
-		                         gan_train_data.generated_mask, true );
-		smooth_discriminator_fake_acc = smooth_discriminator_fake_acc < 0 ? discriminator_fake_acc : 0.99 *
-		                                smooth_discriminator_fake_acc + 0.01
-		                                * discriminator_fake_acc;
-
+													  gan_train_data.generated_mask, true );
+													  
+		smooth_discriminator_fake_acc = isNaNInf ( discriminator_fake_acc ) ? smooth_discriminator_fake_acc :
+										smooth_discriminator_fake_acc < 0 ? discriminator_fake_acc : 0.99 *
+										smooth_discriminator_fake_acc + 0.01
+										* discriminator_fake_acc;
+										
 		// loss gen
-		smooth_generator_loss = smooth_generator_loss < 0 ? generator_loss : 0.99 * smooth_generator_loss + 0.01
-		                        * generator_loss;
+		smooth_generator_loss = isNaNInf ( generator_loss ) ? smooth_generator_loss :
+								smooth_generator_loss < 0 ? generator_loss : 0.99 * smooth_generator_loss + 0.01
+								* generator_loss;
 		// set generator grads
 		gan_train_data.generator_dy.x = discriminator->layers[0]->dx.array().rowwise() * gan_train_data.generated_mask.row (
-		                                    0 ).array();
+											0 ).array();
 		nn->layers.back()->y = nn->layers.back()->y.array().rowwise() * gan_train_data.generated_mask.row ( 0 ).array();
-
-
+		
+		
 		// generator backward pass
 		nn->backward ( -gan_train_data.generator_dy.x );
-
+		
 		// update discriminator weights
 		discriminator->update ( discriminator->learning_rate, discriminator->decay );
-
+		
 		// update generator weights
 		nn->update ( nn->learning_rate, nn->decay );
-
+		
 		iters++;
-
-
-
+		
+		
+		
 		// update graph data
 		if ( iters % 100 == 0 ) {
-
+		
 			update_graph_data ( discriminator->loss_data, smooth_generator_loss );
 			update_graph_data ( discriminator->real_acc_data, smooth_discriminator_real_acc );
 			update_graph_data ( discriminator->fake_acc_data, smooth_discriminator_fake_acc );
-
+			
 		}
-
+		
 		if ( iters % 1000 == 0 ) {
-
+		
 			discriminator->clock = true;
 			gl_data->updated();
 			nn->collect_statistics();
 			discriminator->collect_statistics();
 		}
-
+		
 		usleep ( 100 );
-
+		
 	}
-
+	
 	return 0;
-
+	
 }
 
 int main ( int /* argc */, char ** /* argv */ ) {
 
 	try {
-
+	
 		/* init GUI */
 		nanogui::init();
 		screen = std::shared_ptr<GUI> ( new GUI() );
-
+		
 		// launch a compute thread
 		std::thread compute_thread ( compute );
-
+		
 		nanogui::mainloop ( 1 );
-
+		
 		compute_thread.join();
 		nanogui::shutdown();
-
-	} catch ( const std::runtime_error &e ) {
-
+		
+	}
+	catch ( const std::runtime_error &e ) {
+	
 		std::string error_msg = std::string ( "std::runtime_error: " ) + std::string ( e.what() );
 		return -1;
-
+		
 	}
-
+	
 	return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,8 +1,8 @@
 /*
 * @Author: kmrocki@us.ibm.com
 * @Date:   2017-03-03 15:22:47
-* @Last Modified by:   kmrocki@us.ibm.com
-* @Last Modified time: 2017-04-11 12:54:39
+* @Last Modified by:   Kamil Rocki
+* @Last Modified time: 2017-04-13 17:23:01
 */
 
 #ifndef __UTIL_MAIN_H__
@@ -23,51 +23,51 @@
 #include <Eigen/Dense>
 
 
-std::string return_current_time_and_date(const char* format = "%x %X") {
+std::string return_current_time_and_date ( const char *format = "%x %X" ) {
 
 	auto now = std::chrono::system_clock::now();
-	auto in_time_t = std::chrono::system_clock::to_time_t(now);
-
+	auto in_time_t = std::chrono::system_clock::to_time_t ( now );
+	
 	std::stringstream ss;
-	ss << std::put_time(std::localtime(&in_time_t), format);
+	ss << std::put_time ( std::localtime ( &in_time_t ), format );
 	return ss.str();
 }
 
 template <typename T>
-std::string to_string_with_precision(const T a_value, const int m = 12, const int n = 5) {
+std::string to_string_with_precision ( const T a_value, const int m = 12, const int n = 5 ) {
 	std::ostringstream out;
-	out << std::fixed << std::setw( m ) << std::setprecision( n ) << std::setfill( '_' ) << a_value;
+	out << std::fixed << std::setw ( m ) << std::setprecision ( n ) << std::setfill ( '_' ) << a_value;
 	return out.str();
 }
 
-std::string string_format(const std::string fmt_str, ...) {
-	int final_n, n = ((int)fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
+std::string string_format ( const std::string fmt_str, ... ) {
+	int final_n, n = ( ( int ) fmt_str.size() ) * 2; /* Reserve two times as much as the length of the fmt_str */
 	std::string str;
 	std::unique_ptr<char[]> formatted;
 	va_list ap;
-	while (1) {
-		formatted.reset(new char[n]); /* Wrap the plain char array into the unique_ptr */
-		strcpy(&formatted[0], fmt_str.c_str());
-		va_start(ap, fmt_str);
-		final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
-		va_end(ap);
-		if (final_n < 0 || final_n >= n)
-			n += abs(final_n - n + 1);
+	while ( 1 ) {
+		formatted.reset ( new char[n] ); /* Wrap the plain char array into the unique_ptr */
+		strcpy ( &formatted[0], fmt_str.c_str() );
+		va_start ( ap, fmt_str );
+		final_n = vsnprintf ( &formatted[0], n, fmt_str.c_str(), ap );
+		va_end ( ap );
+		if ( final_n < 0 || final_n >= n )
+			n += abs ( final_n - n + 1 );
 		else
 			break;
 	}
-	return std::string(formatted.get());
+	return std::string ( formatted.get() );
 }
 
-std::vector<std::string> split(std::string in) {
+std::vector<std::string> split ( std::string in ) {
 	std::vector<std::string> strings;
-	std::istringstream f(in);
+	std::istringstream f ( in );
 	std::string s;
-	while (getline(f, s, ' ')) {
+	while ( getline ( f, s, ' ' ) ) {
 		std::cout << s << std::endl;
-		strings.push_back(s);
+		strings.push_back ( s );
 	}
-
+	
 	return strings;
 }
 
@@ -78,41 +78,42 @@ float rand_float ( float mn, float mx ) {
 	return mn + ( mx - mn ) * r;
 }
 
-Eigen::Matrix4f quat_to_mat(const Eigen::Quaternionf &q) {
+Eigen::Matrix4f quat_to_mat ( const Eigen::Quaternionf &q ) {
 
 	Eigen::Matrix4f mat = Eigen::Matrix4f::Identity();
-	mat.block(0, 0, 3, 3) = q.matrix();
+	mat.block ( 0, 0, 3, 3 ) = q.matrix();
 	return mat;
-
+	
 }
 
-Eigen::Matrix4f translate(const Eigen::Vector3f &v) {
-	return Eigen::Affine3f(Eigen::Translation<float, 3>(v)).matrix();
+Eigen::Matrix4f translate ( const Eigen::Vector3f &v ) {
+	return Eigen::Affine3f ( Eigen::Translation<float, 3> ( v ) ).matrix();
 }
 
-Eigen::Quaternionf rotate(const Eigen::Vector3f &angle, const Eigen::Vector3f &forward, const Eigen::Vector3f &up, const Eigen::Vector3f &right) {
-
-	Eigen::AngleAxisf roll(angle[0], forward);
-	Eigen::AngleAxisf yaw(angle[1], up);
-	Eigen::AngleAxisf pitch(angle[2], right);
-
+Eigen::Quaternionf rotate ( const Eigen::Vector3f &angle, const Eigen::Vector3f &forward, const Eigen::Vector3f &up,
+							const Eigen::Vector3f &right ) {
+							
+	Eigen::AngleAxisf roll ( angle[0], forward );
+	Eigen::AngleAxisf yaw ( angle[1], up );
+	Eigen::AngleAxisf pitch ( angle[2], right );
+	
 	return yaw * pitch * roll;
 }
 
-void histogram(Eigen::MatrixXf& m, Eigen::VectorXf* h) {
+void histogram ( Eigen::MatrixXf &m, Eigen::VectorXf *h ) {
 
 	h->setZero();
-
+	
 	// float _min_ = m.minCoeff();
 	// float _max_ = m.maxCoeff();
-
+	
 	// float bin_size = (_max_ - _min_ / (float)h->size());
-
+	
 	// std::cout << bin_size;
-
+	
 	float bin_size = 0.2f;
 	float min_val = -1.0f;
-
+	
 	// x - 0.8
 	// - 0.8 - 0.6
 	// - 0.6 - 0.4
@@ -123,42 +124,40 @@ void histogram(Eigen::MatrixXf& m, Eigen::VectorXf* h) {
 	// 0.4 0.6
 	// 0.6 0.8
 	// 0.8 -
-
+	
 	// std::cout << min_val << std::endl;
-
-	for (size_t ii = 0; ii < m.size(); ii++) {
-
-		float val = m(ii);
-		int bin_id = (val - min_val) / bin_size;
-		if (bin_id > (h->size() - 1)) bin_id = h->size() - 1;
-		if (bin_id < 0) bin_id = 0;
-		h->operator()(bin_id) += 1;
-
+	
+	for ( size_t ii = 0; ii < m.size(); ii++ ) {
+	
+		float val = m ( ii );
+		int bin_id = ( val - min_val ) / bin_size;
+		if ( bin_id > ( h->size() - 1 ) ) bin_id = h->size() - 1;
+		if ( bin_id < 0 ) bin_id = 0;
+		h->operator() ( bin_id ) += 1;
+		
 	}
-
+	
 	float _max_hist_ = h->maxCoeff();
-
-	for (size_t ii = 0; ii < h->size(); ii++) {
-
-		h->operator()(ii) /= _max_hist_;
-
-	}
-
-}
-void checkNaNInf(Eigen::MatrixXf &m) {
-
-	m = m.unaryExpr([](float elem) { // changed type of parameter
-		return (std::isnan(elem) || std::isinf(elem)) ? 0.0 : elem; // return instead of assignment
-	});
-
+	
+	for ( size_t ii = 0; ii < h->size(); ii++ )
+	
+		h->operator() ( ii ) /= _max_hist_;
+		
+		
 }
 
-void checkNaNInf(Eigen::VectorXf &v) {
+bool isNaNInf ( float f ) {
 
-	v = v.unaryExpr([](float elem) { // changed type of parameter
-		return (std::isnan(elem) || std::isinf(elem)) ? 0.0 : elem; // return instead of assignment
-	});
+	return ( std::isnan ( f ) || std::isinf ( f ) );
+	
+}
 
+void checkNaNInf ( Eigen::MatrixXf &m ) {
+
+	m = m.unaryExpr ( [] ( float elem ) { // changed type of parameter
+		return ( std::isnan ( elem ) || std::isinf ( elem ) ) ? 0.0 : elem; // return instead of assignment
+	} );
+	
 }
 
 #endif
